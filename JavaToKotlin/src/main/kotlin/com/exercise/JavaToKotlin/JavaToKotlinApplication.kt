d3a9fef739af4e043c964771ab02c108aad8df02
Pooja Shreedhar <poojashreedhar068@gmail.com>
2024-09-23 17:17:02 +1000

    initialize kotlin

---
+++ JavaToKotlin/src/main/kotlin/com/exercise/JavaToKotlin/JavaToKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.exercise.JavaToKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaToKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<JavaToKotlinApplication>(*args)
+}
